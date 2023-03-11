f4958f3d08d5be5a2dd51e2ee2f72bb2db2e2c6a
MikhailoSalko <slipknotmisha@gmail.com>
2023-03-11 12:20:16 +0200

    remove extra code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,25 +5,8 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 function handleTimePlayer(event) {
-  // console.log('played the video!');
-  // console.log(e.seconds);
   localStorage.setItem('videoplayer-current-time', event.seconds);
 }
 
 player.on('timeupdate', throttle(handleTimePlayer, 1000));
 player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
-
-// .then(function (seconds) {
-//   seconds = localStorage.getItem('videoplayer-current-time');
-// })
-// .catch(function (error) {
-//   switch (error.name) {
-//     case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//       break;
-
-//     default:
-//           // some other error occurred
-//       break;
-//   }
-// });
