b0ef63d9ab4c07b36765287594b97eb69dc56780
MikhailoSalko <slipknotmisha@gmail.com>
2023-03-12 11:14:42 +0200

    update solution to the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+player.on('timeupdate', throttle(handleTimePlayer, 1000));
+
 function handleTimePlayer(event) {
   localStorage.setItem('videoplayer-current-time', event.seconds);
 }
 
-player.on('timeupdate', throttle(handleTimePlayer, 1000));
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const currentTime = localStorage.getItem('videoplayer-current-time')
+  ? localStorage.getItem('videoplayer-current-time')
+  : 0;
+
+player.setCurrentTime(currentTime);
